9d938c5d0e870e441ddc08d6b748f51d9a775cda
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-06 12:12:43 +0200

    Update var.js

--- var.js
+++ var.js
@@ -6,8 +6,12 @@ allowed;
 
 var age = 13;
 var accompanied = false;
+
+var allowed = (age >= 13) || ((age < 13) && accompanied);
 allowed;
 
 var age = 12;
 var accompanied = false;
+
+var allowed = (age >= 13) || ((age < 13) && accompanied);
 allowed;
